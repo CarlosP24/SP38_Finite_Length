3bd439739178fca7248c81b00a941d47b6e75e87
Carlos Payá <carlos.paya@csic.es>
2024-05-29 11:56:57 +0200

    TCM_10 redo

--- job_julia.jl
+++ job_julia.jl
@@ -40,11 +40,11 @@ include("calcs/calc_length.jl")
 mod = ARGS[1]
 L = parse(Int64, ARGS[2])
 
-#calc_LDOS(mod, L; Φrng, ωrng, Zs)
+calc_LDOS(mod, L; Φrng, ωrng, Zs)
 
 Φrng = subdiv(-10, 3.5, Φlength*2)
-#calc_LDOS(mod, L; Φrng, ωrng, Zs = 0, nforced = 1)
-#calc_LDOS(mod, L; Φrng, ωrng, Zs = 0, nforced = 3)
+calc_LDOS(mod, L; Φrng, ωrng, Zs = 0, nforced = 1)
+calc_LDOS(mod, L; Φrng, ωrng, Zs = 0, nforced = 3)
 
 
 if L == 0
